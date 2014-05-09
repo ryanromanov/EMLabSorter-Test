c3a5a30e2e24b2f6bbfd246e7a44bf5a5c4858b3
ryanromanov <ryanromanosky@icloud.com>
2014-05-09 13:38:11 -0400

    CSS migration, navbar cust., centered pics, MySQL
    Finished MySQL for entering votes and displaying them
    
    centered pics for displaying
    
    customized navbar
    
    migrated CSS to main.css

--- process.php
+++ process.php
@@ -4,14 +4,25 @@ $con=mysqli_connect("localhost","root", "passw0rd", "cbiTest");
 if (mysqli_connect_errno($con)) {
 	echo mysqli_connect_error() . "Connection Failed! You've made a huge mistake..."; 
 }
+
 $email = mysqli_real_escape_string($con, $_POST['email']);
 	$dupes = "SELECT * FROM emailCheck WHERE Email='$email'";
-	
+	$picSelect = $_POST['picSelect'];
+	foreach ($picSelect as $a) {
+//		$selectSqlPic = "SELECT * FROM pictureVotes WHERE ID='$a'";
+//		$sqlPicResult = mysqli_query($con, $selectSqlPic);
+		$ballot = "UPDATE pictureVotes SET Votes = Votes+1 WHERE ID = '$a'";
+		$countBallot = mysqli_query($con, $ballot);
+		echo $a . "\n";
+	}
 	$result = mysqli_query($con, $dupes);
 	if (mysqli_num_rows($result) > 0) {
 		echo "Some imposter has already stolen your email for voting.";
 		
 	} else {
+	
+		
+		//echo($picSelect);
 		$sql="INSERT INTO emailCheck (Email)
 		VALUES ('$email')";
 		
