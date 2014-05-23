c1f83191bd4efe6890a0e51ed40e3c90a4d3c400
ryanromanov <ryanromanosky@icloud.com>
2014-05-23 11:22:37 -0400

    validate went to javascript, fixed process php

--- process.php
+++ process.php
@@ -17,6 +17,7 @@ $email = mysqli_real_escape_string($con, $_POST['email']);
 	if (mysqli_num_rows($result) > 0) {
 		die('Error: ' . mysqli_error($con));
 		echo "Some imposter has already voted with your email.";
+		return false;
 		
 		
 	} else {
@@ -24,7 +25,7 @@ $email = mysqli_real_escape_string($con, $_POST['email']);
 		foreach ($picSelect as $a) {
 			$ballot = "UPDATE pictureVotes SET Votes = Votes+1 WHERE ID = '$a'";
 			$countBallot = mysqli_query($con, $ballot);
-			echo $a . "\n";
+//			echo $a . "\n";
 		}
 		//echo($picSelect);
 		$sql="INSERT INTO emailCheck (Email)
@@ -32,6 +33,7 @@ $email = mysqli_real_escape_string($con, $_POST['email']);
 		
 		if (!mysqli_query($con, $sql)) {
 			die('Error: ' . mysqli_error($con));
+			return false;
 		}
 		echo "<br /><br />" . "Your vote has been processed." . "<br /><br />" . "Taking you to the results...";
 	}
